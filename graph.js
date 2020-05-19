809288203249e9b6a0cea2bb72a1e3e308ff0efc
SparkXV <deepanshuvijay123@gmail.com>
2020-05-19 20:50:19 +0530

    Testing

--- graph.js
+++ graph.js
@@ -5,4 +5,14 @@ Store.graph=function(V,E){
     var graph=Object.create(Store.G)
     graph.edges=[]
     graph.vertices=[]
+    graph.vertexIndex=[]
+    graph.autoid=1
+
+    if(Array.isArray(V)) graph.addVertices(V)
+    if(Array.isArray(E)) graph.addEdges(E)
+
+    return graph
+}
+
+Store.G.addVertices=function(vs){ vs.forEach(this.addVertex.bind(this))}
 }
--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-v=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+V=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 E = [ [1,2], [1,3],  [2,4],  [2,5],  [3,6],  [3,7],  [4,8]
     , [4,9], [5,10], [5,11], [6,12], [6,13], [7,14], [7,15] ]
 
-
-parents=function(vertices){
+/*
+function parents(vertices){
     var accum=[]
     for(var i=0;i<E.length;i++){
         var edge=E[i]
@@ -12,7 +12,31 @@ parents=function(vertices){
     }
     return accum
 }
+*/
 
+parents=function(node){
+    var res=[]
+    for(var i=0;i<E.length;i++){
+        var edge=E[i];
+        if(edge[1]===node[0]){
+            res.push(edge[0])
+        }
+    }
+    return res
+}
+children=function(parent){
+    var res=[]
+    for(var j=0;j<parent.length;j++){
+        for(var i=0;i<E.length;i++){
+            var edge=E[i];
+            if(edge[0]===parent[j]){
+                res.push(edge[1])
+            }
+        }
+    }
+    return res
+}
+/*
 parents  = (vertices) => E.reduce( (acc, [parent, child])=> vertices.includes(child)  ? acc.concat(parent) : acc , [] )
 children = (vertices) => E.reduce( (acc, [parent, child])=> vertices.includes(parent) ? acc.concat(child)  : acc , [] )
 
@@ -24,5 +48,7 @@ parents  = function(x) {
 }
 children = function(x) { return E.reduce(
   function(acc, e) { return ~x.indexOf(e[0]) ? acc.concat(e[1]) : acc }, [] )}
-
-console.log(children(children(children(parents(parents(parents([8])))))))
+*/
+//console.log(parents(parents([14])))
+//console.log(children(children(children(parents(parents(parents([8])))))))
+console.log(children(children((parents(parents([12]))))))
